58f9ed40b3ceccb54ae74a22fa67f357346693ae
loidpadre <loidpadre@gmail.com>
2025-01-09 21:35:57 -0300

    vercel config

--- src/app/api/thought/route.tsx
+++ src/app/api/thought/route.tsx
@@ -1,20 +1,30 @@
 import { connect } from "@/app/utils/dataBase";
-import { Data } from "@/data";
-import { PropsData } from "@/app/page";
-import { error } from "console";
 import { NextResponse } from "next/server";
 import { Post } from "../models/post";
 
 export async function GET() {
+  // Estabelecer a conexão com o banco de dados
+  await connect();
 
-    await connect()
-  
+  // Buscar os dados no banco
   const thought = await Post.find();
 
-  if (!thought) {
-    return NextResponse.json("Sem Pensamento");
+  // Criando a resposta
+  const response = NextResponse.json(thought || { message: "Sem Pensamento" });
+
+  // Adicionando cabeçalhos CORS para permitir requisições da Vercel
+  response.headers.set('Access-Control-Allow-Origin', 'https://africanthoughts-com.vercel.app/');
+  response.headers.set('Access-Control-Allow-Origin', 'https://africanthoughts-r7lanzhym-loid-padres-projects.vercel.app');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Se não encontrar nenhum pensamento, retornar com a mensagem
+  if (!thought || thought.length === 0) {
+    NextResponse.json({ message: "Sem Pensamento" });
+    return response;
   }
-  return NextResponse.json(thought);
+
+  // Se encontrar dados, retorna normalmente
+  NextResponse.json(thought);
+  return response;
 }
-
-
